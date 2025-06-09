dbfb589b274e306a88636f0fc0d2d57c9d4b7c5a
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-09 19:44:31 -0300

    Mision dos commented

--- juego-pba/src/MisionDos.java
+++ juego-pba/src/MisionDos.java
@@ -5,37 +5,41 @@ public class MisionDos {
     Mapa mapa;
     Snake snake;
     Personaje[] enemigos;
-    boolean c4Agarrado = false;
-    Posicion posC4;
-    Posicion posPuerta;
+    boolean agarroElC4 = false;
+    Posicion posicionC4;
+    Posicion posicionPuerta;
 
     public MisionDos() {
         mapa = new Mapa(9, 9);
+        // coloca a snake en la posicion (4,4)
         snake = new Snake(4, 4);
         mapa.colocarPersonaje(snake);
 
-        posC4 = new Posicion(1, 1);
-        mapa.colocarItem(new Item("C4"), posC4.x, posC4.y);
+        // coloca el c4 en la posicion (1,1)
+        posicionC4 = new Posicion(1, 1);
+        mapa.colocarItem(new Item("C4"), posicionC4.x, posicionC4.y);
 
-        posPuerta = new Posicion(7, 7);
+        // coloca la puerta de salida en la posicion (7,7)
+        posicionPuerta = new Posicion(7, 7);
 
         Random rand = new Random();
-        int cantEnemigos = 4;
-        enemigos = new Personaje[cantEnemigos];
+        int cantidadEnemigos = 4;
+        enemigos = new Personaje[cantidadEnemigos];
 
-        for (int i = 0; i < cantEnemigos; i++) {
-            int ex, ey;
+        for (int i = 0; i < cantidadEnemigos; i++) {
+            int filaEnemigo, columnaEnemigo;
             do {
-                ex = rand.nextInt(9);
-                ey = rand.nextInt(9);
-            } while (distancia(ex, ey, snake.getPosicion().x, snake.getPosicion().y) < 2 || !mapa.celdas[ex][ey].estaLibre());
+                filaEnemigo = rand.nextInt(9);
+                columnaEnemigo = rand.nextInt(9);
+            } while (distancia(filaEnemigo, columnaEnemigo, snake.getPosicion().x, snake.getPosicion().y) < 2 || !mapa.celdas[filaEnemigo][columnaEnemigo].estaLibre());
 
-            Guardia g = new Guardia(ex, ey);
-            enemigos[i] = g;
-            mapa.colocarPersonaje(g);
+            Guardia guard = new Guardia(filaEnemigo, columnaEnemigo);
+            enemigos[i] = guard;
+            mapa.colocarPersonaje(guard);
         }
     }
 
+    // calcula la distancia entre dos puntos
     public int distancia(int x1, int y1, int x2, int y2) {
         int diferenciaX = x1 - x2;
         if (diferenciaX < 0) {
@@ -48,57 +52,85 @@ public class MisionDos {
         return diferenciaX + diferenciaY;
     }
 
-    private boolean enemigosCercaDePuerta() {
+    // esta funcion es reemplazada por la de abajo
+    // lo vemos en discord cual dejamos
+
+    /*private boolean enemigosCercaDePuerta() {
         for (int i = 0; i < enemigos.length; i++) {
             Posicion p = enemigos[i].getPosicion();
-            if (distancia(p.x, p.y, posPuerta.x, posPuerta.y) <= 3) {
+            if (distancia(p.x, p.y, posicionPuerta.x, posicionPuerta.y) <= 3) {
                 return true;
             }
         }
+        return false;
+    }*/
+
+    private boolean enemigosCercaDePuerta() {
+        // iterar sobre todos los enemigos del mapa
+        for (int i = 0; i < enemigos.length; i++) {
+            Posicion posicionEnemigo = enemigos[i].getPosicion();
+
+            // calcular  la distancia entre el enemigo y la puerta
+            int distanciaAPuerta = distancia(posicionEnemigo.x, posicionEnemigo.y,
+                    posicionPuerta.x, posicionPuerta.y);
+
+            // si el enemigo está a 3 celdas o menos, retornar true
+            if (distanciaAPuerta <= 3) {
+                return true;
+            }
+        }
+
+        // caso contrario retorna false
         return false;
     }
 
     public void jugar() {
         Scanner sc = new Scanner(System.in);
         while (true) {
-            mapa.imprimirMapaConPuerta(snake.getPosicion(), enemigos, posPuerta);
-            System.out.println("Explosivo C4 obtenido: " + c4Agarrado);
+            mapa.imprimirMapaConPuerta(snake.getPosicion(), enemigos, posicionPuerta);
+            System.out.println("Explosivo C4 obtenido: " + agarroElC4);
             System.out.println("Mover Snake: w=arriba, s=abajo, a=izquierda, d=derecha");
             String input = sc.nextLine();
-            int dx = 0, dy = 0;
+            int movimientoFila = 0, movimientoColumna = 0;
             switch (input) {
-                case "w": dx = -1;
+                // movimiento hacia arriba
+                case "w": movimientoFila = -1;
                 break;
-                case "s": dx = 1;
+                // movimiento hacia abajo
+                case "s": movimientoFila = 1;
                 break;
-                case "a": dy = -1;
+                // movimiento hacia izquierda
+                case "a": movimientoColumna = -1;
                 break;
-                case "d": dy = 1;
+                // movimiento hacia derecha
+                case "d": movimientoColumna = 1;
                 break;
                 default:
                     System.out.println("Entrada inválida");
                     continue;
             }
 
-            int nx = snake.getPosicion().x + dx;
-            int ny = snake.getPosicion().y + dy;
-            if (!mapa.esPosicionValida(nx, ny)) {
+            int nuevaFila = snake.getPosicion().x + movimientoFila;
+            int nuevaColumna = snake.getPosicion().y + movimientoColumna;
+            // en caso de que se termine el mapa, el movimiento será inválido
+            if (!mapa.esPosicionValida(nuevaFila, nuevaColumna)) {
                 System.out.println("Movimiento fuera del mapa");
                 continue;
             }
-            if (mapa.estaOcupado(nx, ny)) {
+
+            if (mapa.estaOcupado(nuevaFila, nuevaColumna)) {
                 System.out.println("Celda ocupada, movimiento inválido");
                 continue;
             }
 
             mapa.celdas[snake.getPosicion().x][snake.getPosicion().y].personaje = null;
-            snake.posicion = new Posicion(nx, ny);
-            mapa.celdas[nx][ny].personaje = snake;
+            snake.posicion = new Posicion(nuevaFila, nuevaColumna);
+            mapa.celdas[nuevaFila][nuevaColumna].personaje = snake;
 
-            if (!c4Agarrado && nx == posC4.x && ny == posC4.y && mapa.celdas[nx][ny].item != null) {
-                Item it = mapa.recogerItem(nx, ny);
+            if (!agarroElC4 && nuevaFila == posicionC4.x && nuevaColumna == posicionC4.y && mapa.celdas[nuevaFila][nuevaColumna].item != null) {
+                Item it = mapa.recogerItem(nuevaFila, nuevaColumna);
                 if (it.tipo.equals("C4")) {
-                    c4Agarrado = true;
+                    agarroElC4 = true;
                     System.out.println("Has agarrado el explosivo C4.");
                 }
             }
@@ -118,8 +150,8 @@ public class MisionDos {
                 }
             }
 
-            if (snake.getPosicion().equals(posPuerta)) {
-                if (!c4Agarrado) {
+            if (snake.getPosicion().equals(posicionPuerta)) {
+                if (!agarroElC4) {
                     System.out.println("Debes agarrar el explosivo C4 antes de salir.");
                 } else if (enemigosCercaDePuerta()) {
                     System.out.println("No puedes salir, hay enemigos cerca de la puerta (a menos de 3 celdas).");
